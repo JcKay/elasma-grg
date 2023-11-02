0800124117506764b92c5722e70a096c7714d3d0
JcKay <kay.jangchul@gmail.com>
2023-11-03 00:15:32 +0630

    elasma 2.1.1 [stable]

--- ElasMaGRG.py
+++ ElasMaGRG.py
@@ -9,29 +9,30 @@ from ElasIndex import ElasIndex
 from ElasCompare import ElasCompare
 from ElasPut import PutDoc
 
-processing = True
-while processing:
-    current_time = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
-    target_time = "2023-10-25 23:58:00"
 
-    if current_time == target_time:
-        # print("It's 11:55 PM UTC. Running the provided code.")
+# processing = True
+# while processing:
+#     current_time = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+#     target_time = "2023-10-25 23:58:00"
+#
+#     if current_time == target_time:
+#         # print("It's 11:55 PM UTC. Running the provided code.")
 
-        def check_directory(directory='logs/'):
-            if not os.path.exists(directory):
-                os.mkdir(directory)
+def check_directory(directory='/root/elasma-grg/logs/'):
+    if not os.path.exists(directory):
+        os.mkdir(directory)
 
 
-        # Connect to Elastic and get data
-        connector = ElasConnect().elas_connect()
-        data = connector.cat.indices(index="*", format="json")
-        modules = ElasModules().elas_modules
-        index_data = ElasIndex(data, modules).get_index_data()
+# Connect to Elastic and get data
+connector = ElasConnect().elas_connect()
+data = connector.cat.indices(index="*", format="json")
+modules = ElasModules().elas_modules
+index_data = ElasIndex(data, modules).get_index_data()
 
-        check_directory()
+check_directory()
 
-        dps = ElasCompare(index_data)
-        dps.compare_index()
-        dps.add_tdps()
-        PutDoc("grg", connector).put_doc()
-        processing = False
+dps = ElasCompare(index_data)
+dps.compare_index()
+dps.add_tdps()
+PutDoc("grg", connector).put_doc()
+# processing = False
